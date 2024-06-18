de00ca6f33953a555d25359e1f29037989a9f764
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-06-18 12:45:16 +0200

    Update comandos.js

--- bot/comandos/comandos.js
+++ bot/comandos/comandos.js
@@ -51,7 +51,7 @@ export function comandosInfo(botInfo){
                     "*Comandos executados* : {p4}\n"+
                     "*Contato do criador* : wa.me/{p5}\n"+
                     "*Versão atual* : {p6}\n"+
-                    "*Facebook* :+265990117051\n"
+                    "*Facebook* :265990117051\n"
                 }
             }
         },
