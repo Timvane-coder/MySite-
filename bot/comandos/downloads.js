ae0fa969c99165742b784658d74816c0d3c58bd0
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-07-02 01:20:48 +0200

    Update downloads.js

--- bot/comandos/downloads.js
+++ bot/comandos/downloads.js
@@ -40,14 +40,14 @@ export const downloads = async(c, mensagemBaileys, botInfo) => {
                 }
                 break
             
-            case "mp4":
+            case "yt":
                 try{
                     if(!args.length) return await socket.responderTexto(c,id_chat,erroComandoMsg(comando, botInfo),mensagem)
                     let usuarioTexto = texto_recebido
                     const {resultado : resultadoInfoVideo} = await api.Downloads.obterInfoVideoYT(usuarioTexto)
-                    if(resultadoInfoVideo.isLiveContent) return await socket.responderTexto(c, id_chat, comandos_info.downloads.mp4.msgs.erro_live, mensagem)
-                    else if(resultadoInfoVideo.lengthSeconds > 3000000) return await socket.responderTexto(c, id_chat, comandos_info.downloads.mp4.msgs.limite, mensagem)
-                    const mensagemEspera = criarTexto(comandos_info.downloads.mp4.msgs.espera, resultadoInfoVideo.title, resultadoInfoVideo.durationFormatted)
+                    if(resultadoInfoVideo.isLiveContent) return await socket.responderTexto(c, id_chat, comandos_info.downloads.yt.msgs.erro_live, mensagem)
+                    else if(resultadoInfoVideo.lengthSeconds > 3000000) return await socket.responderTexto(c, id_chat, comandos_info.downloads.yt.msgs.limite, mensagem)
+                    const mensagemEspera = criarTexto(comandos_info.downloads.yt.msgs.espera, resultadoInfoVideo.title, resultadoInfoVideo.durationFormatted)
                     await socket.responderTexto(c, id_chat, mensagemEspera, mensagem)
                     const {resultado : resultadoYTMP4} = await api.Downloads.obterYTMP4(resultadoInfoVideo.videoId)
                     await socket.responderArquivoBuffer(c, tiposMensagem.video, id_chat, resultadoYTMP4, '', mensagem, 'video/mp4')
